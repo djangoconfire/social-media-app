d2f0b7896f6385c1358be92bdbb8843482617e35
djangoconfire <djangopycon@gmail.com>
2019-07-29 13:04:57 +0530

    added new route for like , unlike , comment and delete scream

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,42 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
-const { getAllScreams, postScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const {
+	getAllScreams,
+	postScream,
+	getScream,
+	deleteScream,
+	likeScream,
+	unlikeScream,
+	commentOnScream
+} = require('./handlers/screams');
+
+const {
+	signup,
+	login,
+	uploadImage,
+	addUserDetails,
+	getAuthenticatedUser,
+	getUserDetails
+} = require('./handlers/users');
+
 const FirebaseAuthMiddleware = require('./util/fbAuth');
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/createScream', FirebaseAuthMiddleware, postScream);
+app.get('/scream/:screamId', getScream);
+app.delete('/scream/:screamId', FirebaseAuthMiddleware, deleteScream);
+app.get('/scream/:screamId/like', FirebaseAuthMiddleware, likeScream);
+app.get('/scream/:screamId/unlike', FirebaseAuthMiddleware, unlikeScream);
+app.post('/scream/:screamId/comment', FirebaseAuthMiddleware, commentOnScream);
 
 // User routes
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FirebaseAuthMiddleware, uploadImage);
+app.post('/user', FirebaseAuthMiddleware, addUserDetails);
+app.get('/user', FirebaseAuthMiddleware, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
 
 exports.api = functions.https.onRequest(app);
--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -9,21 +9,20 @@ module.exports = (req, res, next) => {
 		return res.status(403).json({ error: 'Unauthorized' });
 	}
 
-	// Verifiy token
 	admin
 		.auth()
 		.verifyIdToken(idToken)
 		.then((decodedToken) => {
 			req.user = decodedToken;
-			console.log('Decoded Token', decodedToken);
 			return db.collection('users').where('userId', '==', req.user.uid).limit(1).get();
 		})
 		.then((data) => {
 			req.user.handle = data.docs[0].data().handle;
+			req.user.imageUrl = data.docs[0].data().imageUrl;
 			return next();
 		})
 		.catch((err) => {
-			console.error('Error while verifying token', err);
+			console.error('Error while verifying token ', err);
 			return res.status(403).json(err);
 		});
 };
